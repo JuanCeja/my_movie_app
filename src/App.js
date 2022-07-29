5d14e5b1fbd389183f4e49cc167088fc8e6daf33
JuanCeja <jmc347ls2@yahoo.com>
2022-07-28 20:49:20 -0700

    added comments for better understanding

--- src/App.js
+++ src/App.js
@@ -8,17 +8,23 @@ const API_URL = `http://www.omdbapi.com/?apikey=7bceea39`;
 
 const App = () => {
 
+    // this is our state variables and set state functions
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
 
+    // searchMovies function will make the call to the API using title as the parameter
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
+        // we console.log our data to see how its coming back and whats coming back
         console.log(data);
+
+        // we use our setMovies state function to set our movies into an array of movies(objects) that our searchMovies is bringing back from the API
         setMovies(data.Search);
     }
 
+    // use effect will call our searchMovies function on render
     useEffect(() => {
         searchMovies('war');
     }, [])
@@ -44,10 +50,12 @@ const App = () => {
                     ? (
                         <div className='container'>
                             {movies.map((movie) => (
+                                // we map threw our array of movies and pass each movie(object) as props to our MovieCard Component
                                 <MovieCard movie={movie}/>
                             ))}
                         </div>
                     ) : (
+                        // if we have no movies to display we return this
                         <div className='empty'>
                             <h2>No movies found.</h2>
                         </div>
